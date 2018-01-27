0e3cc7b36b9aa3a170d8153bc6002d279dcc1e62
Pickle9 <35817092+Pickle9@users.noreply.github.com>
2018-01-27 19:58:21 +0300

    Task4

--- src/by/htp/hw2/task4/Solution.java
+++ src/by/htp/hw2/task4/Solution.java
@@ -1,7 +1,44 @@
 package by.htp.hw2.task4;
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
+        double a;
+        double b;
+        double c;
 
+        a = readDouble();
+        b = readDouble();
+
+        if (a == b) {
+            System.out.println("Скоро Новый Год!");
+        }
+        else {
+            c = readDouble();
+
+            System.out.println(a + b + c);
+            System.out.println(Math.pow(a, 2) + Math.pow(b, 2));
+            System.out.println("Моя любимая футбольная команда.");
+        }
+    }
+
+    @SuppressWarnings("all")
+    private static double readDouble() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число:");
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                return scanner.nextDouble();
+            } else {
+                System.out.println("Неверное значение, попробуйте ещё раз.");
+                scanner.next();
+            }
+        }
+
+        return 0;
     }
 }
