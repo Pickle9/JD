9b808006e85efc7b9e9bb5b8ee030d6ac2fe6d42
Pickle9 <35817092+Pickle9@users.noreply.github.com>
2018-01-27 17:43:20 +0300

    Task2

--- src/by/htp/hw2/task2/Solution.java
+++ src/by/htp/hw2/task2/Solution.java
@@ -2,50 +2,46 @@ package by.htp.hw2.task2;
 
 import java.util.Scanner;
 
-/*Программа запрашивает у пользователя a, b, c, вычислить значение выражения a^2-(b-c)^2+ln(b^(2)+1)
+/*Программа запрашивает у пользователя a, b, c, вычислить значение выражения a^2-(b-c)^2+ln(b^(2)+1).
 * */
 
 public class Solution {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double a = 0;
-        double b = 0;
-        double c = 0;
+
+        double a;
+        double b;
+        double c;
         double res;
 
         System.out.println("Введите переменную \"а\".");
+        a = readDouble();
+
+        System.out.println("Введите переменную \"b\".");
+        b = readDouble();
+
+        System.out.println("Введите переменную \"b\".");
+        c = readDouble();
+
+        res = Math.pow(a, 2) - Math.pow((b - c), 2) + Math.log(Math.pow(b, 2) + 1);
+
+        System.out.println("Результат: " + res);
+    }
+
+    private static double readDouble() {
+
+        Scanner scanner = new Scanner(System.in);
+
         while (scanner.hasNext()) {
             if (scanner.hasNextDouble()) {
-                a = scanner.nextDouble();
+                return scanner.nextDouble();
             } else {
-                System.out.println("Неверное значение, введите переменную \"а\"");
-                scanner.next();
-                continue;
-            }
-
-            System.out.println("Введите переменную \"b\".");
-            if (scanner.hasNextDouble()) {
-                b = scanner.nextDouble();
-            } else {
-                System.out.println("Неверное значение, введите переменную \"а\"");
-                scanner.next();
-                continue;
-            }
-
-            System.out.println("Введите переменную \"c\".");
-            if (scanner.hasNextDouble()) {
-                c = scanner.nextDouble();
-                break;
-            } else {
-                System.out.println("Неверное значение, введите переменную \"а\"");
+                System.out.println("Неверное значение, попробуйте ещё раз.");
                 scanner.next();
             }
         }
 
-        res = Math.pow(a, 2) - Math.pow((b - c), 2) + Math.log(Math.pow(b, 2) + 1);
-
-        System.out.println("Результат" + res);
-
+        return 0;
     }
 }
 
