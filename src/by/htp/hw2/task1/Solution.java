4e498fba1fcd67329d293635f06d5f182d31986b
Pickle9 <35817092+Pickle9@users.noreply.github.com>
2018-01-29 00:27:56 +0300

    Task1

--- src/by/htp/hw2/task1/Solution.java
+++ src/by/htp/hw2/task1/Solution.java
@@ -1,18 +1,46 @@
 package by.htp.hw2.task1;
 
-/*Задание №1.
-Напишите программу нахождения гипотенузы и плозади прямоугольного треугольника по двум катетам.*/
+/* Напишите программу нахождения гипотенузы и площади прямоугольного треугольника по двум катетам.*/
 
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        double a = 3.0;   //Катет a.
-        double b = 3.0;   //Катет b.
-        double hyp = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));   //Расчёт гипотенузы.
-        double square = (a * b)/2;   //Расчёт площади
 
-        System.out.println(String.format("Гипотенуза треугольника с катетами %o и %o = %f", (int)a, (int)b, hyp));   //Для более красивого кода использую String.format,
-                                                                                                                    // а для красивых цифр при выводе на экран привожу a и b к int-у.
+    public static void main(String[] args) {
+
+        double a;
+        double b;
+        double hyp;
+        double square;
+
+        System.out.println("Введите катет \"а\":");
+        a = readDouble();
+
+        System.out.println("Введите катет \"b\":");
+        b = readDouble();
+
+        hyp = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));   //Расчёт гипотенузы.
+        square = (a * b) / 2;   //Расчёт площади
+
+        System.out.println(String.format("Гипотенуза треугольника с катетами %o и %o = %f", (int) a, (int) b, hyp));
         System.out.println("А площадь данного треугольника = " + square);
+    }
+
+    //Чтение сканнером числа(int) с консоли с "защитой от дураков".
+    @SuppressWarnings("all")
+    private static double readDouble() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                return scanner.nextDouble();
+            } else {
+                System.out.println("Неверное значение, попробуйте ещё раз.");
+                scanner.next();
+            }
+        }
+
+        return 0;
     }
 }
